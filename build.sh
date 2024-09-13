578662bd47f2ea9a5668882f2d191f2e06d65dd8
LeandroPanozzo <leandropanozzo1@gmail.com>
2024-09-13 16:04:57 -0300

    usuario

--- build.sh
+++ build.sh
@@ -10,12 +10,7 @@ python manage.py collectstatic --no-input
 
 # Aplica las migraciones pendientes
 python manage.py migrate
-
-# Crear un superusuario automáticamente si no existe
-if [ -z "$(echo "from django.contrib.auth import get_user_model; User = get_user_model(); print(User.objects.filter(is_superuser=True).exists())" | python manage.py shell)" ]; then
-    echo "Creando superusuario..."
-    python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@example.com', 'adminpassword')"
-else
-    echo "Superusuario ya existe."
-fi
-# 'admin', 'admin@example.com' y 'adminpassword'
+export DJANGO_SUPERUSER_USERNAME = admin
+export DJANGO_SUPERUSER_EMAIL=test@test.com
+export DJANGO_SUPERUSER_PASSWORD=testpass123498765
+ python manage.py createsuperuser --no-input
